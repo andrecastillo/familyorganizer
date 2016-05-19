f95161dd57f44ce766f36106a532977385c644d3
Andre Castillo <andrecastillo@gmail.com>
2016-05-19 21:56:36 +0000

    blah

--- js/config.js
+++ js/config.js
@@ -42,6 +42,10 @@ var config = {
 		defaultSymbol: 'calendar', // Fontawsome Symbol see http://fontawesome.io/cheatsheet/
         urls: [
 		{
+			symbol: 'calendar-plus-o', 
+			url: 'https://p01-calendarws.icloud.com/ca/subscribe/1/n6x7Farxpt7m9S8bHg1TGArSj7J6kanm_2KEoJPL5YIAk3y70FpRo4GyWwO-6QfHSY5mXtHcRGVxYZUf7U3HPDOTG5x0qYnno1Zr_VuKH2M'
+		},
+		{
 			symbol: 'soccer-ball-o',
 			url: 'webcal://mlb.am/tix/marlins_schedule_full',
 		},
